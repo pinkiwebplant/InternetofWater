a679b2e7f4f2e9522ef92cc853b9b104798e3270
rakeshm-twp <rakeshm@thewebplant.in>
2025-02-11 12:38:03 +0530

    js local Updates Done

--- src/js/main.js
+++ src/js/main.js
@@ -32,41 +32,6 @@ window.addEventListener('scroll', function () {
 	}
 });
 
-const searchInput = document.querySelector('.et_search_outer form input[type=text]')
-document.getElementById("et_top_search").addEventListener("click", function(b) {
-	document.body.classList.toggle("search-open");
-	setTimeout(function(){
-		searchInput.focus()
-	},400)
-	b.stopPropagation();
-});
-
-// Handle click on body and .et_search_outer form button to remove 'search-open' class
-document.body.addEventListener("click", function() {
-	document.body.classList.remove("search-open");
-});
-document.querySelector(".et_search_outer form button").addEventListener("click", function(e) {
-	e.preventDefault();
-	document.body.classList.remove("search-open");
-});
-
-// Prevent click propagation on #main-header
-document.getElementById("main-header").addEventListener("click", function(b) {
-	b.stopPropagation();
-});
-
-
-document.querySelector(".mobile_menu_bar_toggle").addEventListener("click", function() {
-	document.documentElement.classList.toggle("menu-open");
-});
-
-window.addEventListener('scroll', function() {
-	if (window.scrollY >= 41) {
-		document.body.classList.add('fixed-header');
-	} else {
-		document.body.classList.remove('fixed-header');
-	}
-});
 
 var images = document.querySelectorAll('img.hs-image-widget');
 Array.prototype.slice.call(images).forEach(function (img, index) {
@@ -100,3 +65,51 @@ $(function() {
 		}
 	});
 });
+
+
+
+window.addEventListener('scroll', function() {
+	if (window.scrollY >= 41) {
+		document.body.classList.add('fixed-header');
+	} else {
+		document.body.classList.remove('fixed-header');
+	}
+});
+
+
+const searchInput = document.querySelector('.et_search_outer form input[type=text]');
+const searchButton = document.getElementById("et_top_search");
+
+if (searchInput && searchButton) {
+    searchButton.addEventListener("click", function(b) {
+        document.body.classList.toggle("search-open");
+        setTimeout(function() {
+            searchInput.focus();
+        }, 400);
+        b.stopPropagation();
+    });
+}
+
+// Handle click on body and .et_search_outer form button to remove 'search-open' class
+document.body.addEventListener("click", function() {
+	document.body.classList.remove("search-open");
+});
+document.querySelector(".et_search_outer form button").addEventListener("click", function(e) {
+	e.preventDefault();
+	document.body.classList.remove("search-open");
+});
+
+// Prevent click propagation on #main-header
+document.getElementById("main-header").addEventListener("click", function(b) {
+	b.stopPropagation();
+});
+
+
+document.querySelector(".mobile_menu_bar_toggle").addEventListener("click", function() {
+    document.documentElement.classList.toggle("menu-open");
+});
+
+document.querySelector("#et_top_search").addEventListener("click", function() {
+    document.documentElement.classList.remove("menu-open");
+});
+
