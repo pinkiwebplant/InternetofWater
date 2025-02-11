bf8c78bcb63637b3477dd7a875b8978cb77953e7
SarvjeetSingh13 <sarvjeet@thewebplant.in>
2025-02-11 12:22:05 +0530

    Video Gallery

--- src/modules/u4m-video-gallery.module/module.js
+++ src/modules/u4m-video-gallery.module/module.js
@@ -1,35 +1,31 @@
-$(function() { 
-// Card's slider
-  var $carousel = $('.slider-for');
-
-  $carousel
-    .slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: true,
-      fade: true,
-      adaptiveHeight: true,
-      asNavFor: '.slider-nav'
-    })
-    
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    asNavFor: '.slider-for',
-    dots: false,
-    centerMode: false,
-    focusOnSelect: true,
-    variableWidth: false,
-    responsive: [
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true
-        }
-      }
-    ]
-  });
+// Primary slider.
+var primarySlider = new Splide('#primary_slider', {
+    type: 'fade',
+    heightRatio: 0.5,
+    pagination: false,
+    arrows: false,
+    cover: true,
 });
+
+// Thumbnails slider.
+var thumbnailSlider = new Splide('#thumbnail_slider', {
+    rewind: true,
+      type: 'loop',
+    isNavigation: true,
+    gap: 10,
+    focus: 'center',
+    pagination: false,
+     perPage: 3,
+  perMove: 3,
+    cover: true,
+    breakpoints: {
+        '991': {
+            fixedWidth: 66,
+            fixedHeight: 40,
+        }
+    }
+}).mount();
+
+// sync the thumbnails slider as a target of primary slider.
+primarySlider.sync(thumbnailSlider).mount();
 
